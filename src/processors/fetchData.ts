7fdd9ce3cd17a8f25636b585ce0ab30828045a18
Benjamin Vincent <luxcium@neb401.com>
2024-12-05 15:36:23 -0500

    Optimize promise execution in Pipeline and PromiseQueue
    Add `createUnawaitedPromise` method to `Pipeline` class and update processors.
    
    * **Pipeline Class Changes**
      - Add `createUnawaitedPromise` method to create and enqueue unawaited promises.
      - Modify `executeSequentially` and `executeInParallel` methods to use `createUnawaitedPromise`.
      - Update `executeUnawaited` method to use `createUnawaitedPromise`.
    
    * **Pipeline Test Changes**
      - Add test for `createUnawaitedPromise` method.
      - Update tests for `executeSequentially` and `executeInParallel` to use `createUnawaitedPromise`.
    
    * **Processor Changes**
      - Remove `promiseQueue.enqueue` call from `fetchData`, `processData`, and `saveData` processors.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/Luxcium/image-pipeline?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/processors/fetchData.ts
+++ src/processors/fetchData.ts
@@ -1,12 +1,10 @@
 import { Processor } from './Processor';
 import { DataContainer } from '../containers/DataContainer';
-import { promiseQueue } from '../pipeline/PromiseQueue';
 
 export const fetchData: Processor<string, any> = (container) => {
   const promise = fetch(container.data)
     .then(response => response.json())
     .then(data => new DataContainer(data));
 
-  promiseQueue.enqueue(promise);
   return promise;
 };
--- src/processors/processData.ts
+++ src/processors/processData.ts
@@ -1,6 +1,5 @@
 import { Processor } from './Processor';
 import { DataContainer } from '../containers/DataContainer';
-import { promiseQueue } from '../pipeline/PromiseQueue';
 
 export const processData: Processor<any, any> = (container) => {
   const promise = new Promise<DataContainer<any>>((resolve) => {
@@ -8,6 +7,5 @@ export const processData: Processor<any, any> = (container) => {
     resolve(new DataContainer(processedData));
   });
 
-  promiseQueue.enqueue(promise);
   return promise;
 };
