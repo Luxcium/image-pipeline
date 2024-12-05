88befa3369af1fe73b1300076c740c53c0a94c67
Benjamin Vincent <luxcium@neb401.com>
2024-12-05 14:30:59 -0500

    Remove redundant `PromiseQueue` class definition and import `PromiseQueue` from `src/pipeline/PromiseQueue.ts` in `Pipeline.ts`
    * Remove the redundant `PromiseQueue` class definition
    * Import the `PromiseQueue` class from `src/pipeline/PromiseQueue.ts`
    * Modify the `Pipeline` class to utilize the imported `PromiseQueue`
    
    Add tests for the `Pipeline` class in `Pipeline.test.ts`
    
    * Add tests for sequential and parallel execution of processors
    * Include tests for `fetchData`, `processData`, and `saveData` processors

--- src/pipeline/Pipeline.ts
+++ src/pipeline/Pipeline.ts
@@ -1,29 +1,6 @@
 import { Processor } from '../processors/Processor';
 import { Container } from '../containers/Container';
-
-class PromiseQueue {
-  private queue: Promise<any>[] = [];
-
-  enqueue<T>(promise: Promise<T>): void {
-    this.queue.push(promise);
-  }
-
-  async executeAllSequential(): Promise<void> {
-    for (const promise of this.queue) {
-      await promise;
-    }
-    this.queue = [];
-  }
-
-  async executeAllParallel(): Promise<void> {
-    await Promise.all(this.queue);
-    this.queue = [];
-  }
-
-  reset(): void {
-    this.queue = [];
-  }
-}
+import { PromiseQueue } from './PromiseQueue';
 
 const promiseQueue = new PromiseQueue();
 
